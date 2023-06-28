358d393deb726f6b33e4ab82f387754a9d155b17
James Anuncio <anunciojames14@gmail.com>
2023-06-28 17:59:26 +0800

    created AuthService for all Auth methods, (login, refresh, logout)

--- app/Http/Services/AuthService.php
+++ app/Http/Services/AuthService.php
@@ -2,30 +2,46 @@
 
 namespace App\Http\Services;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthService {
 
     // check for cookie and removes the refreshToken in the database and the cookie.
-    public function checkCookie($refreshTokenCookie){
+    public function checkCookie($refreshTokenCookie, $requestMethod, $user = null)
+    {
+   
+        if (!$refreshTokenCookie) 
+            return false; 
 
-        if ($refreshTokenCookie) {
-            $refreshToken = PersonalAccessToken::findToken($refreshTokenCookie);
+        
+        $refreshToken = PersonalAccessToken::findToken($refreshTokenCookie);
+      
+    
+        if (!$refreshToken)
+            return false;
 
-            if ($refreshToken) {
-                $refreshToken->delete(); // Remove the refresh token from the database
-                cookie()->forget('refresh_token');
-            }
-        }
-
-        return;
+        // Token has an expiry date and it has already passed, consider it expired
+        if ($refreshToken->expires_at && Carbon::parse($refreshToken->expires_at)->isPast() || 
+              ($user && $refreshToken->tokenable->email !== $user->email)) {
+              // Remove the refresh token from the database
+              $refreshToken->delete();          
+              return true;
+          } else {
+              if ($requestMethod === 'login') {
+                  $refreshToken->delete();          
+              } else {
+                  return $refreshTokenCookie; // Return the associated user
+              }
+          }
     }
 
     public function login($credentials, $refreshTokenCookie)
     {
         // Check for cookie and remove the cookie
-        $this->checkCookie($refreshTokenCookie);
+        $checkCookie = $this->checkCookie($refreshTokenCookie, 'login');
     
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
@@ -33,6 +49,7 @@ class AuthService {
             // Create access and refresh tokens
             $accessToken = $this->createTokens($user, 'access');
             $refreshToken = $this->createTokens($user, 'refresh');
+          
     
             // Save the refresh token as an HTTP-only cookie
             $cookie = cookie('refresh_token', $refreshToken, 7 * 24 * 60); // 1 week expiration
@@ -48,22 +65,45 @@ class AuthService {
             'error' => 'Invalid credentials',
         ];
     }
+    public function refresh($refreshTokenCookie)
+    {
+        $user = Auth::user();
+      
+
+       $checkRefreshToken = $this->checkCookie($refreshTokenCookie, 'refresh', $user);
+   
+       // if checkRefreshToken is false or true it means that the refresh token has expired or has already been deleted in the database and refresh miss use
+       if(!$checkRefreshToken || $checkRefreshToken === true)
+       return [
+        'error' => 'Invalid token, please sign in again',
+        ];
+
+
+        $accessToken = $this->createTokens($user, 'access');
+
+        return [
+            'user' => $user,
+            'access_token' => $accessToken,
+            'cookie' => $checkRefreshToken
+        ];
+
+    }
 
   //creates access or refresh token basing on the token type
   private function createTokens($user, $type)
   {
-    $tokenType = $type == 'access'  ? 'Access Token' : 'Refresh Token';
-    $expiryDate = $type == 'access' ? now()->addMinutes(15) : now()->addWeek();
-        
-    //create token access/refresh token
-    $accessToken = $user->createToken($tokenType, [$tokenType]);
-    $accessToken->accessToken->expires_at = $expiryDate;
-    $accessToken->accessToken->save();
-   
-   
-   return $accessToken->plainTextToken;
-     
-  }
+      $tokenType = $type == 'access' ? 'Access Token' : 'Refresh Token';
+      $expiryDate = $type == 'access' ? now()->addMinutes(15) : now()->addWeek();
   
+      // Create access/refresh token
+      $accessToken = $user->createToken($tokenType, [$tokenType]);
+      $accessToken->accessToken->expires_at = $expiryDate;
+      $accessToken->accessToken->save();
+  
+      if($type === 'access')
+      return $accessToken->plainTextToken;
+      else
+      return $accessToken->plainTextToken;
+  }  
 
 }
